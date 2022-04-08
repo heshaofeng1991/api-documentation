c4141ceefe57beb4a1945493bfa145a52277062d
Zhanbing Han <zhanbing.han@gmail.com>
2022-04-08 19:10:21 +0800

    update

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,19 +1,14 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/NextSmartShip/openapi/develop/gen/http/openapi3.yaml?token=GHSAT0AAAAAABR7UQ72IY7UXKKB2PKPYEKMYSWOROA",
-    dom_id: '#swagger-ui',
+    url: "https://raw.githubusercontent.com/NextSmartShip/openapi/develop/gen/http/openapi3.yaml?token=GHSAT0AAAAAABSTE5SEZUEARAMADCG3QSV4YSZKJQA",
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
