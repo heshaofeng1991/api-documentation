19464a6ab77764bf07136044984e094f9145c137
johnnysfhe <johnny.he@nextsmartship.com>
2022-04-25 20:28:48 +0800

    api

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/NextSmartShip/openapi-documention/master/openapi3.yaml?token=GHSAT0AAAAAABR7UQ72SEKYFOUGWPQZBYK4YTGRORQ",
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
